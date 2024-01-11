f597f8fbb1ad1c688ceb71f5934d2599ec2ca7a1
Husrol <147827107+Husrol@users.noreply.github.com>
2024-01-11 21:28:20 +0300

    Update SaveData.py

--- SaveData.py
+++ SaveData.py
@@ -1,1 +1,19 @@
-
+import csv
+import UpdateListBox
+def save_data(name,name_to_be_recorded_1,name_to_be_recorded_2,name_to_be_recorded_3,balance_sheet,income_statement,cash_flow):
+    with open("List of Corps.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([name])
+    with open(f"{name_to_be_recorded_1}.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in balance_sheet:
+            writer.writerow(row)
+    with open(f"{name_to_be_recorded_2}.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in income_statement:
+            writer.writerow(row)
+    with open(f"{name_to_be_recorded_3}.csv", 'w', newline='') as file:
+        writer = csv.writer(file)
+        for row in cash_flow:
+            writer.writerow(row)
+    UpdateListBox.update()
