362843027dbca44fd27c20685083e76fb8e7da08
İrem Beyza Gül <139780921+irembeyzagl@users.noreply.github.com>
2024-01-11 17:42:39 +0000

    Pending changes exported from your codespace

---
+++ Interface.py
@@ -0,0 +1,17 @@
+#import libs
+import tkinter as tk
+from tkinter import ttk
+from tkinter import Tk, Listbox, Button, Toplevel,messagebox
+import requests
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import numpy as np
+from array import *
+import Scrapping as sc
+import ScrapLive
+import pandas as pd
+import csv
+import UpdateListBox
+import UpdateData
